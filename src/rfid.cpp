4f0e451c6c3d08080063b9e9ce3cfe392f833b09
Evan Haas <159657777+evan15h@users.noreply.github.com>
2024-04-15 00:18:48 -0400

    Update rfid.cpp

--- src/rfid.cpp
+++ src/rfid.cpp
@@ -1,23 +1,26 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include "rfid.h"
 
 // Define the pins used for the SPI connection of the RFID reader
 #define RST_PIN 2  // Reset pin for the RFID reader
-#define SS_PIN  15 // Slave select pin for the SPI connection
+#define SS_PIN  5 // Slave select pin for the SPI connection
+#define SCK_PIN 18
+#define MISO_PIN 19
+#define MOSI_PIN 23
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create an instance of the MFRC522 class
 
-void setup() {
+void rfidSetup() {
   Serial.begin(115200); // Initialize serial communications with the PC
   while (!Serial);      // Do nothing if no serial port is opened
 
-  SPI.begin();          // Init SPI bus
+  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
+          // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522 card
 
   Serial.println(F("Scan PICC to see UID..."));
 }
 
-void loop() {
+void rfidLoop() {
     // Look for new cards
     if (!mfrc522.PICC_IsNewCardPresent()) {
         return;
@@ -37,14 +40,14 @@ void loop() {
     Serial.println();
 
     // Example UIDs of known cards
-    const byte knownCard1[4] = {0xAA, 0xBB, 0xCC, 0xDD}; // Replace these with your card's UID
-    const byte knownCard2[4] = {0x11, 0x22, 0x33, 0x44}; // Replace these with your card's UID
+    const byte tag1[4] = {0x73, 0xB2, 0x65, 0xFA}; // Replace these with your card's UID
+    const byte knownCard1[4] = {0xD3, 0x8C, 0x65, 0x1A}; // Replace these with your card's UID
 
     // Check if the detected card matches one of the known UIDs
-    if (memcmp(mfrc522.uid.uidByte, knownCard1, 4) == 0) {
+    if (memcmp(mfrc522.uid.uidByte, tag1, 4) == 0) {
+        Serial.println("Hello, Tag 1!");
+    } else if (memcmp(mfrc522.uid.uidByte, knownCard1, 4) == 0) {
         Serial.println("Hello, Card 1!");
-    } else if (memcmp(mfrc522.uid.uidByte, knownCard2, 4) == 0) {
-        Serial.println("Hello, Card 2!");
     } else {
         Serial.println("Unknown card detected.");
     }
